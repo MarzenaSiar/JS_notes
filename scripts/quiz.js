3a1774d69476327cc0c700d83207a36d84f4768c
Marzena Siarkiewicz <marzena.siarkiewicz.ab@gmail.com>
2023-10-16 18:37:06 +0200

    KMA

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -1,9 +1,70 @@
-const dt = []
+const data = [
+    {
+        name: 'pawel',
+        city: 'krakow',
+        age: 38,
+        hobbies: ['js', 'python', 'drugs']
+    },
+    {
+        name: 'pawel',
+        cite: 'Wroclaw',
+        age: '37',
+        hobbies: ['js', 'drugs', 'eat']
+    },
+    {
+        name: 'pawel',
+        city: 'chelm',
+        age: '24',
+        hobbies: ['tennis', 'sleep', 'gym']
+    },
+    {
+        name: "iza",
+        city: "warsaw",
+        age: 28,
+        hobbies: ["learning languages", "traveling", "js"]
+    },
+    {
+        name: 'marzena',
+        city: 'warsaw',
+        age: 31,
+        hobbies: ["js", "books", "travelling"],
+    },
+    {
+        name: 'adam',
+        city: 'pisz',
+        age: 19,
+        hobbies: ['guitar', 'gym', 'html']
+    },
+    {
+        name: "kamila",
+        city: "gdynia",
+        age: 37,
+        hobbies: ["diving", "eating", "js"]
+    },
+    {
+        name: 'tomek',
+        city: 'zabrze',
+        age: 40,
+        hobbies: ['paintball']
+    },
+    {
+        name: 'dawid',
+        city: 'tarnów',
+        age: 23,
+        hobbies: ['js', 'beer', 'css']
+    },
+    {
+        name: "olga",
+        city: "warszawa",
+        age: 34,
+        hobbies: ["js", "cats", "swimming"]
+    },
+    {
+        name: 'kasia',
+        city: 'warszawa',
+        age: 29,
+        hobbies: ['programowanie', 'sport']
+    }
+]
 
-x = {
-    age: 31,
-    city: "warsaw",
-    sex: "woman",
-    hobbies: "js", "books", "travelling",
-    name: "marzena"
-}
+console.log(data.length);
