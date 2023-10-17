6a1ddbcd6f345e3b63c4f2baa4b47a05146db3a9
Marzena Siarkiewicz <marzena.siarkiewicz.ab@gmail.com>
2023-10-17 10:57:07 +0200

    AverageAge

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -8,13 +8,13 @@ const data = [
     {
         name: 'pawel',
         cite: 'Wroclaw',
-        age: '37',
+        age: 37,
         hobbies: ['js', 'drugs', 'eat']
     },
     {
         name: 'pawel',
         city: 'chelm',
-        age: '24',
+        age: 24,
         hobbies: ['tennis', 'sleep', 'gym']
     },
     {
@@ -64,7 +64,32 @@ const data = [
         city: 'warszawa',
         age: 29,
         hobbies: ['programowanie', 'sport']
+    },
+    {
+        name: 'dominika',
+        city: 'warszawa',
+        age: 40,
+        hobbies: ['cycling', 'dreaming', 'car driving']
     }
 ]
 
-console.log(data.length);
+// console.log(data.length);
+
+// policz średnią wieku
+
+function calculateAvgAge(people) {
+    let sum = 0;
+
+    // for (let i = 0; i < people.length; i++) {
+    //     sum += people[i].age
+    // }
+
+    for (const person of people) {
+        sum += person.age;
+    }
+
+    return sum / people.length;
+}
+
+const result = calculateAvgAge(data);
+console.log(result);
